ccb6c4ab669d65a707e204feffb8575adb9e50f2
prayagupd <prayag.upd@gmail.com>
2017-07-13 00:04:28 -0700

    add starting compute cloud

--- src/main/groovy/ElasticCloudComputing.groovy
+++ src/main/groovy/ElasticCloudComputing.groovy
@@ -3,6 +3,8 @@ import com.amazonaws.regions.Region
 import com.amazonaws.regions.Regions
 import com.amazonaws.services.ec2.AmazonEC2Client
 import com.amazonaws.services.ec2.model.DescribeInstancesRequest
+import com.amazonaws.services.ec2.model.StartInstancesRequest
+
 /**
  * Created by prayagupd
  * on 7/12/17.
@@ -20,6 +22,24 @@ class ElasticCloudComputing {
         this.getClass().getResource( 'application.properties' ).withInputStream {
             properties.load(it)
         }
+    }
+
+    def start(def instanceId, def region) {
+        if(local) {
+            System.setProperty("aws.profile", properties."authentication.profile")
+        }
+
+        def elasticCoumputing = new AmazonEC2Client(new DefaultAWSCredentialsProviderChain())
+        elasticCoumputing.setRegion(Region.getRegion(Regions.fromName(region)))
+
+        elasticCoumputing.startInstances(new StartInstancesRequest(instanceIds: [instanceId]))
+                .startingInstances.instanceId
+
+        while (describe(instanceId, region) != "running") {
+            println("waiting for elastic compute cloud to run")
+        }
+
+        return describe(instanceId, region)
     }
 
     //@CompileStatic
--- src/test/groovy/ElasticCloudComputingSpecs.groovy
+++ src/test/groovy/ElasticCloudComputingSpecs.groovy
@@ -7,10 +7,15 @@ import spock.lang.Specification
 
 class ElasticCloudComputingSpecs extends Specification {
 
-    //this test is stupid because I'm testing existing instance id
+    def computing = new ElasticCloudComputing(true)
 
+    def "starts a VM"() {
+        expect:
+        assert computing.start("i-0243b169c603df734", "us-west-2") == "running"
+    }
+
+    //this test is stupid because I'm testing existing instance id
     def "describes an instance"() {
-        def computing = new ElasticCloudComputing(true)
 
         expect:
         assert computing.describe("i-04aebf8153af9e518", "us-west-2") == "running"
